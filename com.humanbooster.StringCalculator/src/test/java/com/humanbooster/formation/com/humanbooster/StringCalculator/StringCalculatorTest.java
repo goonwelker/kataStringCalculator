8eb9cfcc45300bcb91df1d83eab84d68ea9767db
Victor Moccand <victor.moccand@gmail.com>
2016-03-10 12:30:57 +0100

    dernier commit

--- com.humanbooster.StringCalculator/src/test/java/com/humanbooster/formation/com/humanbooster/StringCalculator/StringCalculatorTest.java
+++ com.humanbooster.StringCalculator/src/test/java/com/humanbooster/formation/com/humanbooster/StringCalculator/StringCalculatorTest.java
@@ -8,26 +8,24 @@ import junit.framework.Assert;
 
 public class StringCalculatorTest {
 
-	//test un param vide
+	// test un param vide
 	@Test
 	public void Add_EmptyStringAsParam_ReturnZero() {
 		int resultEmpty = StringCalculator.Add("");
 		assertEquals(0, resultEmpty);
 	}
 
-	//test param un nombre
+	// test param un nombre
 	@Test
 	public void Add_OneNumberAsParam_ReturnNumber() {
 		int resultOneNumber = StringCalculator.Add("1");
 		assertEquals(1, resultOneNumber);
 	}
-	
+
 	@Test
 	public void Add_TwoNumbersAsParam_ReturnAdditionNumbers() {
 		int resultAdditionNumbers = StringCalculator.Add("1,2");
 		assertEquals(3, resultAdditionNumbers);
 	}
-	
-	
 
 }
