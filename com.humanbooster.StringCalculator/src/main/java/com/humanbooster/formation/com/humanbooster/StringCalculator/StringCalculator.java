350bee624eedfb0dc10f6c7d810f8dc57b47c17e
Victor Moccand <victor.moccand@gmail.com>
2016-03-10 11:26:13 +0100

    refector code

--- com.humanbooster.StringCalculator/src/main/java/com/humanbooster/formation/com/humanbooster/StringCalculator/StringCalculator.java
+++ com.humanbooster.StringCalculator/src/main/java/com/humanbooster/formation/com/humanbooster/StringCalculator/StringCalculator.java
@@ -1,24 +1,16 @@
 package com.humanbooster.formation.com.humanbooster.StringCalculator;
 
 public class StringCalculator {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	public static int Add (String numbers) {
-		String[] splitNumbers=numbers.split(",");
-		if(numbers.equals("")){
+	public static int Add(String numbers) {
+		String[] splitNumbers = numbers.split(",");
+		if (numbers.isEmpty()) {
 			return 0;
-		}else if(splitNumbers.length==2){
-			return Integer.parseInt(splitNumbers[0])+ Integer.parseInt(splitNumbers[1]);
-		}
-		else{
+		} else if (splitNumbers.length == 2) {
+			return Integer.parseInt(splitNumbers[0]) + Integer.parseInt(splitNumbers[1]);
+		} else {
 			return Integer.parseInt(numbers);
 		}
-			
-		
+
 	}
 
 }
